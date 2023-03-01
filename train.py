d2c69180a36a9bb7fadb866d1d479f7776bbfb0f
Ali Yazdizadeh <ali.yazdizadeh.pres@gmail.com>
2023-02-28 22:06:51 -0500

    Add files via upload

--- train.py
+++ train.py
@@ -54,19 +54,27 @@ def main():
     # TODO: Create TabularDataset using TabularDatasetFactory
     # Data is located at:
     # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+    ds = TabularDatasetFactory.from_delimited_files("https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")
 
-    ds = ### YOUR CODE HERE ###
-    
+
     x, y = clean_data(ds)
+
+# TODO: Split data into train and test sets.
+
+    x_train,x_test,y_train,y_test = train_test_split(x,y,random_state=42,test_size=0.25,stratify=y)
 
     # TODO: Split data into train and test sets.
 
     ### YOUR CODE HERE ###a
-
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
 
+    model_path = "./outputs/model.pkl"
+    os.makedirs("outputs", exist_ok=True)
+    print('Saving the model to {}'.format(model_path))
+    joblib.dump(model, model_path)
+    
 if __name__ == '__main__':
     main()
